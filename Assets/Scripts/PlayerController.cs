7c1164ed02b91080036cc69732be97ea37e101d1
KBear100 <kgamett@student.neumont.edu>
2023-10-02 17:52:01 -0600

    Character movement & v1 of map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed;
+
+    Vector2 vel = Vector2.zero;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        vel.x = Input.GetAxis("Horizontal") * speed;
+        vel.y = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = vel;
+    }
+}
