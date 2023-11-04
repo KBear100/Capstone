c454383716924c858314da118c07f7afd31acf33
KBear100 <kgamett@student.neumont.edu>
2023-11-03 19:31:55 -0600

    Intro Sequence

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,11 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] float wait;
+    [SerializeField] GameObject background;
+    [SerializeField] Image backgroundImage;
+    [SerializeField] TMP_Text story;
+
+    float fade = 1.5f;
+    float timer;
+    bool started;
+    bool open;
+
+    private void Start()
+    {
+        background.SetActive(true);
+        story.gameObject.SetActive(false);
+        timer = wait;
+    }
+
+    private void Update()
+    {
+        if (started)
+        {
+            backgroundImage.CrossFadeAlpha(0, fade, true);
+            fade -= Time.deltaTime;
+            if (fade <= 0)
+            {
+                story.text = "A brave adventurer named Meeri left her home town to try and fight the strange being tormenting the land.";
+                background.SetActive(false);
+                story.gameObject.SetActive(true);
+
+                timer -= Time.deltaTime;
+                if(timer <= 0)
+                {
+                    story.text = "You must follow her on her adventure in the great land known as Kear.";
+                    story.CrossFadeAlpha(0, wait, true);
+                    open = true;
+                    timer = wait;
+                }
+            }
+        }
+        
+        if(open)
+        {
+            started = false;
+
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                open = false;
+                OpenGame();
+            }
+        }
+    }
+
     public void StartGame()
+    {
+        started = true;
+        //SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Single);
+    }
+
+    private void OpenGame()
     {
         SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Single);
     }
